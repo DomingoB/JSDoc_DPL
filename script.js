522bd5de5589e3ebb89af9f3a2ec63b7cc85723f
Domingo <domingobdperezpulido@gmail.com>
2024-02-22 17:24:34 +0000

    Se ha introducido los archivos HTML y JS documentado

---
+++ script.js
@@ -0,0 +1,8 @@
+/**
+ * Muestra un saludo personalizado.
+ * @param {string} nombre - El nombre de la persona a saludar.
+ * @returns {void}
+ */
+function saludar(nombre) {
+    alert('¡Hola, ' + nombre + '!');
+}
